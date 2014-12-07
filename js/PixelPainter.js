f15c8e616541dc85a3615014300f2f7bfdc429e0
dmeowmixer <dmeowmixer@hotmail.com>
2014-12-06 18:41:56 -1000

    working on passing value

--- js/PixelPainter.js
+++ js/PixelPainter.js
@@ -1,34 +1,46 @@
 $(function(){
   function PixelPainter(width, height){
-    this.artboard = $("<div>");
-    this.controls = $("<div>");
+      this.artboard = $("<div class = 'grid'>");
+      this.controls = $("<div class = 'colorgrid'>");
 
-  // for (var i = 0; i < width*height -1; i ++){
-  //   var div = $("<div class='test'>",{
-  //   });
-  //   this.artboard.append(div);
-  // };
-  for (var i = 0; i < height; i ++){
-    var row = $("<div class='row'>");
-    for (var j = 0; j < width; j++){
-      var cell = $("<div class='cell'>");
-      row.append(cell);
-    }
-    this.artboard.append(row);
+    for (var i = 0; i < height; i ++){
 
+      var row = $("<div class='row'>");
+
+      for (var j = 0; j < width; j++){
+
+        var cell = $("<div class='cell'>");
+
+        row.append(cell);
+      }
+      this.artboard.append(row);
     }
+
+    for (var k = 0; k < 20; k++){
+
+      var roo = $("<div class ='roo'>");
+
+      for(var h = 0; h < 30; h++){
+
+        var cee = $("<div class = 'cee'>");
+
+        var rgb = [];
+
+        for(var l = 0 ; l < 3; l++){
+          rgb.push(Math.floor(Math.random()*255));
+        }
+        console.log("rgb(" + rgb.join(',')+ ")" )
+        cee.css( 'background-color', "rgb(" + rgb.join(',')+ ")" );
+
+        roo.append(cee);
+      }
+      this.controls.append(roo);
+    };
+    cee.click(function(){
+      var colorhold = $( input ).val();
+    }); 
   }
 
-  // for (var j = 0; j < height; j++){
-  //   var roo = $("<div class='roo'>");
-  //   for (var k = 0; k < width; k++){
-  //     var cee = $("<div class='cee'");
-  //     roo.append(cee);
-  //   }
-  //   this.controls.append(div);
-  //   })
-  // };
-  // }
 
 
   var pixelPainter = new PixelPainter(20,20);
