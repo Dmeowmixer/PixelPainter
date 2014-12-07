6635033dfa2e20bbebb596ff66e5cef6598cd62b
dmeowmixer <dmeowmixer@hotmail.com>
2014-12-06 17:53:33 -1000

    saimin push

--- js/PixelPainter.js
+++ js/PixelPainter.js
@@ -3,19 +3,32 @@ $(function(){
     this.artboard = $("<div>");
     this.controls = $("<div>");
 
-  for (var i = 0; i < width*height -1; i ++){
-    var div = $("<div class='test'>",{
-    });
-    this.artboard.append(div);
-  };
+  // for (var i = 0; i < width*height -1; i ++){
+  //   var div = $("<div class='test'>",{
+  //   });
+  //   this.artboard.append(div);
+  // };
+  for (var i = 0; i < height; i ++){
+    var row = $("<div class='row'>");
+    for (var j = 0; j < width; j++){
+      var cell = $("<div class='cell'>");
+      row.append(cell);
+    }
+    this.artboard.append(row);
 
-
-  for (var j = 0; j < 25*2; j++){
-    var div = $("<div class='set'>", {
-    })
-    this.controls.append(div);
-  };
+    }
   }
+
+  // for (var j = 0; j < height; j++){
+  //   var roo = $("<div class='roo'>");
+  //   for (var k = 0; k < width; k++){
+  //     var cee = $("<div class='cee'");
+  //     roo.append(cee);
+  //   }
+  //   this.controls.append(div);
+  //   })
+  // };
+  // }
 
 
   var pixelPainter = new PixelPainter(20,20);
